7b3f2c97f07f3710200fc153e77d9fc5130779dd
David Ritchey <ritchey0713@gmail.com>
2020-01-30 23:44:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def long_planeteer_calls# code an argument here
   # Your code here
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(item)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
-  
+  item.find do |food_item|
+    cheese_types.include?(food_item)
+  end 
 end
