38b093e7b9d313ac44f33840696272f6cae69449
David Ritchey <ritchey0713@gmail.com>
2020-01-30 23:44:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def roll_call_dwarves# code an argument here
   # Your code here
 end
@@ -13,6 +15,7 @@ end
 def find_the_cheese(item)
   cheese_types = ["cheddar", "gouda", "camembert"]
   item.find do |food_item|
+    binding.pry
     cheese_types.include?(food_item)
   end 
 end
