bad9fffb591e4def1c672dee1e472e0563b2556e
David Ritchey <ritchey0713@gmail.com>
2020-01-30 23:42:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ def long_planeteer_calls# code an argument here
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
